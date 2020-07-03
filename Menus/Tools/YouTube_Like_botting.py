3f78c473f3cf80464e71ff9ee5df1ea219b8957e
John Bell <johnthegod@mail.ru>
2020-07-03 12:58:26 -0400

    Add files via upload

---
+++ Menus/Tools/YouTube_Like_botting.py
@@ -0,0 +1,26 @@
+import requests, webbrowser, os, sys, time, inspect, json
+from datetime import datetime
+def log(value2print):
+    print(" ["+datetime.now().strftime('%H:%M:%S').replace("''", "")+"] " + value2print + " ")
+def loginWithBot(username, password):
+    log("Logging in with",username+":"+password+"...")
+def loadBots(botsfile):
+    filename = (os.path.basename(botsfile)).replace(" ", "")
+    print(" Trying to load Botfile called '" + filename + "'... ")
+    with open(botsfile.replace(" ", ""), 'r') as myfile:
+         for line in myfile:
+            print(line)
+            line = line.replace('\n', '')
+            email = line.split(":")[0]
+            password = line.split(":")[1]
+def askBotFile():
+    print(" Please drag and drop a email:password list, which contains only working GMAIL and YouTube accounts. ")
+    accountsInput = input("  ==> ")
+    if accountsInput is not "":
+        loadBots(accountsInput)
+    else:
+        exit()
+try:
+    askBotFile()
+except KeyboardInterrupt:
+    exit()
